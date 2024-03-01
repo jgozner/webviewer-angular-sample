52240e9bb1299d63bd56574495468c3acc0c6146
jgozner <jgozner@outlook.com>
2024-03-01 10:15:18 -0800

    Updates

--- src/app/without-viewer/without-viewer.component.ts
+++ src/app/without-viewer/without-viewer.component.ts
@@ -11,9 +11,9 @@ export class WithoutViewerComponent {
 
   click() {
     (async function() {
-      Core.setWorkerPath('./lib/core');
-
-      const arrayBuffer = await Core.officeToPDFBuffer('./files/legal-contract.docx', {l: "demo:1686757895540:7d86b1ba030000000049491bef43714654d2dec710ec9c728a35106d71"})
+     await Core.setWorkerPath('./lib/core');
+      
+      const arrayBuffer = await Core.officeToPDFBuffer('./files/legal-contract.docx', {l : "demo:1686757895540:7d86b1ba030000000049491bef43714654d2dec710ec9c728a35106d71"});
       const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
       const url = URL.createObjectURL(blob);
       window.open(url);
